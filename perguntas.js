bac6b37ae82a6bf0b936aa93b9498cd560190da1
0404mari <162703260+0404mari@users.noreply.github.com>
2024-10-16 09:17:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Conhecimentos gerais',
+    'Quem inventou a lâmpada?',
+    'A lâmpada foi inventadapor Thomas Edison em 1879. No dia 21 de outubro, o inventor conseguiu manter a lâmpada acessa durante 48 horas.'
+)
+
+criaCartao(
+    'Charada',
+    'O que a Xuxa foi fazer no bar?',
+    'Beber caSasha.'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual a ciência que estuda a atmosfera da terra e a climatologia?',
+    'A meteorologia é uma ciência que estuda os fenômenos que ocorrem na atmosfera terrestre, com foco no clima e na previsão do tempo.'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual o satélite natural da terra?',
+    'Há dezenas de satélites naturais no Sistema Solar., mas a Lua é o único satélite natural sa Terra.'
+)
